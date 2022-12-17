2fd44bf28cfd12dcf43a1d00246ed51223bd2436
THEO-184 <theophilusboakye47@gmail.com>
2022-12-17 23:03:25 +0000

    control viewport

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import React, { useState } from "react";
-import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import ReactMapGL, {
+	Marker,
+	Popup,
+	ViewState,
+	ViewStateChangeEvent,
+} from "react-map-gl";
 
 type Coordinates = [number, number];
 interface Props<T> {
@@ -10,18 +15,29 @@ interface Props<T> {
 
 const Map = ({ pickupCoordinates, dropofCoordinates }: Props<Coordinates>) => {
 	const [showPickUpPopUp, setShowPickUpPopUp] = useState(false);
+	const [viewPort, setViewPort] = useState<
+		Omit<ViewState, "bearing" | "padding" | "pitch">
+	>({
+		longitude: -0.205874,
+		latitude: 5.614818,
+		zoom: 10,
+	});
 
+	const handleViewState = (e: ViewStateChangeEvent) => {
+		setViewPort({
+			longitude: e.viewState.longitude,
+			latitude: e.viewState.latitude,
+			zoom: e.viewState.zoom,
+		});
+	};
 	return (
 		<>
 			<ReactMapGL
-				initialViewState={{
-					longitude: -0.205874,
-					latitude: 5.614818,
-					zoom: 10,
-				}}
+				{...viewPort}
 				mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
 				style={{ width: "100%", height: "100%" }}
 				boxZoom
+				onMove={handleViewState}
 				mapStyle={"mapbox://styles/mapbox/streets-v11"}
 			>
 				{showPickUpPopUp ? (
