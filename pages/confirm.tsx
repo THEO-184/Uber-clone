1fcc42fcc1bec408aa25c7d2e08fc0a967a0c0c9
Theophilus Boakye <93124888+TheophilusBoakye@users.noreply.github.com>
2022-12-22 15:15:14 +0000

    choose drive

--- pages/confirm.tsx
+++ pages/confirm.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import Map from "../components/Map";
 import { useRouter } from "next/router";
 import { useGetCoordinates } from "../utils/hooks";
+import Image from "next/image";
+import DriveDetails from "../components/DriveDetails";
+import { carList } from "../utils/constants";
 
 const Confirm = () => {
 	const { query } = useRouter();
@@ -22,7 +25,32 @@ const Confirm = () => {
 					dropofCoordinates={dropOffCoordinates?.features[0]?.center}
 				/>
 			</div>
-			<div className="flex-1 bg-blue-400">confirm locations</div>
+			<div className="flex-1 flex flex-col bg-white">
+				<div className="flex-1">
+					<p className="text-center text-gray-500 py-2 px-3 border-b-2 text-sm">
+						Choose a ride, or swipe up for more.
+					</p>
+					<div className="flex flex-col">
+						{carList.map((car, id) => {
+							const { imgUrl, multiplier, service } = car;
+							return (
+								<DriveDetails
+									image={imgUrl}
+									price={20.32}
+									time={`${5 * multiplier} min away`}
+									title={service}
+									key={id}
+								/>
+							);
+						})}
+					</div>
+				</div>
+				<div className="text-white text-center">
+					<button className="bg-black mx-4 py-4 px-4 w-full">
+						Confirm UberX
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 };
